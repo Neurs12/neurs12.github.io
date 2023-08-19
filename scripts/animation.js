c4ccd44b10caa731b1b8df12318152ed7393bacf
Neurs <hoanghuutrung431@gmail.com>
2023-08-19 15:33:18 +0700

    Change animation name

--- scripts/animation.js
+++ scripts/animation.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
-    let fadeIn = document.querySelectorAll(".fade-initial");
+    let fadeIn = document.querySelectorAll(".fade-animation");
     for (let i = 0; i < fadeIn.length; i++) {
         setTimeout(() => {fadeIn[i].style.opacity = 1}, 250 * i);
     };
-    let floatIn = document.querySelectorAll(".fly-initial");
+    let floatIn = document.querySelectorAll(".fly-animation");
     for (let i = 0; i < floatIn.length; i++) {
         setTimeout(() => {floatIn[i].style.transform = "translateY(0)"}, 250 * i);
     };
